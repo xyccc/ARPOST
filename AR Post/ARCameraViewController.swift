6419bba9c69aaa13f740b64bc3d332e2ede4d2ea
yuchong xiang <yvonna@yuchongs-MacBook-Pro.local>
2018-05-11 23:52:10 -0700

    add painting

--- AR Post/ARCameraViewController.swift
+++ AR Post/ARCameraViewController.swift
@@ -12,35 +12,33 @@ import ARKit
 import FacebookLogin
 import FacebookCore
 
-class ARCameraViewController: UIViewController, ARSCNViewDelegate {
+class ARCameraViewController: UIViewController, ARSCNViewDelegate, UIGestureRecognizerDelegate {
     
     var isAdding = false
-    var isPainting = false {
-        didSet {
-            if isPainting {
-                
-            } else {
-                
-            }
-        }
-    }
+//    var isPainting = false {
+//        didSet {
+//            if isPainting {
+//
+//            } else {
+//
+//            }
+//        }
+//    }
+    var isPainting = false;
     var textButton = UIButton()
     var paintButton = UIButton()
     var imageButton = UIButton()
     
     
     @IBOutlet var sceneView: ARSCNView!
-    
-    @objc func didTapTextButton(sender: UIButton!) {
-        print("add sticky notes")
-    }
-    @objc func didTapPaintButton(sender: UIButton!) {
-        isPainting = true
-        print("start painting")
-    }
-    @objc func didTapImageButton(sender: UIButton!) {
-        print("insert image")
-    }
+    let vertBrush = VertBrush()
+    var buttonDown = false
+    var addPointButton : UIButton!
+    var frameIdx = 0
+    var splitLine = false
+    var lineRadius : Float = 0.001
+    var metalLayer: CAMetalLayer! = nil
+    var hasSetupPipeline = false
     
     @IBAction func didTapAddButton(_ sender: UIButton) {
         if isAdding {
@@ -57,6 +55,20 @@ class ARCameraViewController: UIViewController, ARSCNViewDelegate {
         isAdding = !isAdding
     }
     
+    @objc func didTapTextButton(sender: UIButton!) {
+        print("add sticky notes")
+    }
+    @objc func didTapPaintButton(sender: UIButton!) {
+        isPainting = !isPainting
+        if (isPainting) {
+            print("start painting")
+        } else {
+            print("end painting")
+        }
+    }
+    @objc func didTapImageButton(sender: UIButton!) {
+        print("insert image")
+    }
     
     @IBAction func didTapLogoutButton(_ sender: UIButton) {
         let loginManager = LoginManager()
@@ -74,10 +86,12 @@ class ARCameraViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/world.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        metalLayer = self.sceneView.layer as! CAMetalLayer
     
 
         textButton.frame = CGRect(x: 60, y: 550, width: 80, height: 50)
@@ -98,10 +112,29 @@ class ARCameraViewController: UIViewController, ARSCNViewDelegate {
         imageButton.setTitle("Image", for: .normal)
         imageButton.addTarget(self, action: #selector(didTapImageButton(sender:)), for: .touchUpInside)
         
+        let tap = UILongPressGestureRecognizer(target: self, action: #selector(tapHandler))
+        tap.minimumPressDuration = 0
+        tap.cancelsTouchesInView = false
+        tap.delegate = self
+        self.sceneView.addGestureRecognizer(tap)
+        
     }
     
-    @objc func buttonAction(sender: UIButton!) {
-        print("Button tapped")
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return touch.view == gestureRecognizer.view
+    }
+    
+    // called by gesture recognizer
+    @objc func tapHandler(gesture: UITapGestureRecognizer) {
+        
+        // handle touch down and touch up events separately
+        if gesture.state == .began {
+            // do something...
+            buttonTouchDown()
+        } else if gesture.state == .ended { // optional for touch up event catching
+            // do something else...
+            buttonTouchUp()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -126,6 +159,20 @@ class ARCameraViewController: UIViewController, ARSCNViewDelegate {
         // Release any cached data, images, etc that aren't in use.
     }
     
+    @objc func clearDrawing() {
+        vertBrush.clear()
+    }
+    
+    @objc func buttonTouchDown() {
+        if (isPainting) {
+            splitLine = true
+            buttonDown = true
+        }
+    }
+    @objc func buttonTouchUp() {
+        buttonDown = false
+    }
+    
     // MARK: - ARSCNViewDelegate
     
     /*
@@ -136,6 +183,74 @@ class ARCameraViewController: UIViewController, ARSCNViewDelegate {
      return node
      }
      */
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        
+        if ( buttonDown ) {
+            
+            let pointer = getPointerPosition()
+            if ( pointer.valid ) {
+                
+                if ( vertBrush.points.count == 0 || (vertBrush.points.last! - pointer.pos).length() > 0.001 ) {
+                    
+                    var radius : Float = 0.001
+                    
+                    
+                    if ( splitLine || vertBrush.points.count < 2 ) {
+                        lineRadius = 0.001
+                    } else {
+                        
+                        let i = vertBrush.points.count-1
+                        let p1 = vertBrush.points[i]
+                        let p2 = vertBrush.points[i-1]
+                        
+                        radius = 0.001 + min(0.015, 0.005 * pow( ( p2-p1 ).length() / 0.005, 2))
+                        
+                    }
+                    
+                    lineRadius = lineRadius - (lineRadius - radius)*0.075
+                    vertBrush.addPoint(pointer.pos, radius: lineRadius, splitLine:splitLine)
+                    
+                    if ( splitLine ) { splitLine = false }
+                    
+                }
+                
+            }
+            
+        }
+        
+        if ( frameIdx % 100 == 0 ) {
+            print(vertBrush.points.count, " points")
+        }
+        
+        frameIdx = frameIdx + 1
+        
+        //if ( frameIdx % 2 == 0 ) {
+        vertBrush.updateBuffers()
+        //}
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        
+        if ( !hasSetupPipeline ) {
+            // pixelFormat is different if called at viewWillAppear
+            hasSetupPipeline = true
+            vertBrush.setupPipeline(device: sceneView.device!, pixelFormat: self.metalLayer.pixelFormat )
+        }
+        
+        if let commandQueue = self.sceneView?.commandQueue {
+            if let encoder = self.sceneView.currentRenderCommandEncoder {
+                
+                let projMat = float4x4.init((self.sceneView.pointOfView?.camera?.projectionTransform)!)
+                let modelViewMat = float4x4.init((self.sceneView.pointOfView?.worldTransform)!).inverse
+                
+                vertBrush.render(commandQueue, encoder, parentModelViewMatrix: modelViewMat, projectionMatrix: projMat)
+                
+            }
+        }
+        
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -149,6 +264,20 @@ class ARCameraViewController: UIViewController, ARSCNViewDelegate {
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    func getPointerPosition() -> (pos : SCNVector3, valid: Bool, camPos : SCNVector3 ) {
+        
+        guard let pointOfView = sceneView.pointOfView else { return (SCNVector3Zero, false, SCNVector3Zero) }
+        guard let currentFrame = sceneView.session.currentFrame else { return (SCNVector3Zero, false, SCNVector3Zero) }
+        
+        let mat = SCNMatrix4.init(currentFrame.camera.transform)
+        let dir = SCNVector3(-1 * mat.m31, -1 * mat.m32, -1 * mat.m33)
+        
+        let currentPosition = pointOfView.position + (dir * 0.12)
+        
+        return (currentPosition, true, pointOfView.position)
         
     }
 }
